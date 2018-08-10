4bb97e88c033370bea462a3ae237084f37800e15
DiegoGranadosLy <d.granadosly@gmail.com>
2018-08-10 16:13:25 -0600

    Graficas corregidas, falta crearla como una funcion y llamar con los 3 puntos solicitados.

--- tarea2.m
+++ tarea2.m
@@ -7,12 +7,12 @@
 %Graficos con OpenGL
 graphics_toolkit('gnuplot');
 %Aumentamos la precision de salida
-output_precision(10);
+output_precision(20);
 %Entrada del sistema.
 xi = input("Ingrese un valor para x: ")
 %Declaracion de las variables
 h0     = 1;
-lambda = double(1.1);
+lambda = double(0.9);
 xil  = 0;%xi-1
 xip  = 0;%xi+1
 fxil = 0;%f(xi-1)
@@ -26,11 +26,9 @@ error_da   = zeros(1,500);%Vector con el error de la diferencia hacia adelante
 error_dd   = zeros(1,500);%Vector con el error de la diferencia hacia atras
 error_c    = zeros(1,500);%Vector con la diferencia centrada
 
-valorVerdadero = 2*xi*(e^(xi^2))*cos((pi/180)*(e^(xi^2)))
-
 #Ciclo Que define los valores de xi , xi+1 y xi-1
 #ademas de sus evaluaciones en la funcion.
-for i=1:10
+for i=1:500
   if (i==1)
     vector_h(i) = h0;               %Excepcion para i = 1 con  h0 = 1
   else
@@ -38,31 +36,38 @@ for i=1:10
   endif
   xil  = xi - vector_h(i);      %xi-1 = xi-h
   xip  = xi + vector_h(i);      %xi+1 = xi+h
-  fxil = double(sin(e^(xil^2)));%f(xi-1)
-  fxi  = double(sin(e^(xi^2)));  %f(xi)
-  fxip = double(sin(e^(xip^2)));%f(xi+1)
+  fxil = double(sin(e^(xil^2.0)));%f(xi-1)
+  fxi  = double(sin(e^(xi^2.0)));  %f(xi)
+  fxip = double(sin(e^(xip^2.0)));%f(xi+1)
 
   vector_da(i) = double((fxi-fxil)/(xi-xil));
   vector_dd(i) = double((fxip-fxi)/(xip-xi));
-  vector_c (i) = double((fxip-fxil)/(2*vector_h(i)));
+  vector_c (i) = double((fxip-fxil)/(2.0*vector_h(i)));
 
-  error_da(i)  = (valorVerdadero-vector_da(i))/valorVerdadero; %(vv-va)/vv
-  error_dd(i)  = (valorVerdadero-vector_dd(i))/valorVerdadero;
-  error_c (i)  = (valorVerdadero-vector_c(i))/valorVerdadero;
+  if (i==1)
+    error_da(i)  = 0;
+    error_dd(i)  = 0;
+    error_c (i)  = 0;
+  else
+    error_da(i)  = abs(((vector_da(i-1)-vector_da(i))/(vector_da(i)))*100);
+    error_dd(i)  = abs(((vector_dd(i-1)-vector_dd(i))/(vector_dd(i)))*100);
+    error_c (i)  = abs(((vector_c (i-1)-vector_c (i))/(vector_c(i)))*100);
+  endif
   
 endfor
 
 #Definimos las propiedades de la grafica.
-figure(1);
-axis ([1e-15,2,1e-13,10 ]);
-hold on;
-loglog(abs(vector_h),abs(error_da),'r');
-loglog(abs(vector_h),abs(error_dd),'g');
-loglog(abs(vector_h),abs(error_c),'b');
+loglog(vector_h,error_c,'r',vector_h,error_da,'g',vector_h,error_dd,'b');
+axis([10^-15,10^0,10^-13,10^01]);
 xlabel("Diferencia h");
-ylabel("Error aproximado");
-title("Aproximacion por diferencias"); 
-legend("Adelante","Atras","Centrada");
-grid;
-hold off;
+ylabel("Error");
+title("Aproximacion por diferencias");
+legend("Centrada","Delante","Detras");
+grid on;
+
+
+
+
+
+
 
